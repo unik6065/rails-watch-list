cb85a7a5de09509f44d4aa8af581418c8c936817
romain isoz <isozromain@gmail.com>
2023-02-23 14:17:19 +0100

    ajout de la partie lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,43 @@
 require 'open-uri'
 require 'json'
+require 'faker'
 
 puts 'clean database'
 Movie.delete_all
-puts 'add movies to the seed'
+List.delete_all
+Bookmark.delete_all
+
 
 url = 'https://tmdb.lewagon.com/movie/top_rated'
 movies_serialized = URI.open(url).read
 movies = JSON.parse(movies_serialized)['results']
-
+puts 'add movies to the seed'
 movies.each do |movie|
 
-  Movie.create(
+  Movie.create!(
     title: movie['title'],
     overview: movie['overview'],
-    poster_url: movie['poster_path'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",
     rating: movie['vote_average']
   )
 
   puts "created movie #{movie['title']}"
+
+end
+
+puts 'add lists to the seed'
+
+25.times do
+  list = List.create!(
+    name: Faker::Name.unique.name
+  )
+  puts "create list #{list.name}"
+  bookmark = Bookmark.create!(
+    comment: Faker::Lorem.sentence,
+    movie_id: Movie.all.sample.id,
+    list_id: list.id
+  )
+  puts " -- create bookmark #{bookmark.comment}"
 
 end
 
