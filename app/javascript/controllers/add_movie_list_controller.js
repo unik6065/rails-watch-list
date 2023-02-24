49a6e27d3e289bbcc4afcf5678ed1c7885527472
romain isoz <isozromain@gmail.com>
2023-02-24 14:31:38 +0100

    essai push sur scalingo

--- app/javascript/controllers/add_movie_list_controller.js
+++ app/javascript/controllers/add_movie_list_controller.js
@@ -2,7 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add-movie-list"
 export default class extends Controller {
-  connect() {
-    console.log('Hello from add movie controller')
+
+  show(){
+    var element = document.getElementById('add-movie-container');
+    element.classList.add('show');
+    element.classList.remove('hide');
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show]
+  resources :lists, only: %i[index show] do
+    resources :bookmark, only: %i[new create]
+  end
   resources :movies, only: %i[show]
 end
